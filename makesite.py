0002e956c40c5ff575fae20dc0e6d681a0d4540d
Sophie Kretzschmar <kretzschmar@nvd.rwth-aachen.de>
2024-01-24 12:11:58 +0100

    Removed news feed and allowed html and md files in blog folder

--- makesite.py
+++ makesite.py
@@ -203,25 +203,18 @@ def main():
     make_pages('content/[!_]*.html', '_site/{{ slug }}/index.html',
                page_layout, **params)
 
-    # Create blogs.
-    blog_posts = make_pages('content/blog/*.md',
+    # Create blogs; allows HTML and markdown files, filename starting with date.
+    blog_posts = make_pages('content/blog/20*',
                             '_site/blog/{{ slug }}/index.html',
                             post_layout, blog='blog', **params)
-    news_posts = make_pages('content/news/*.html',
-                            '_site/news/{{ slug }}/index.html',
-                            post_layout, blog='news', **params)
 
     # Create blog list pages.
     make_list(blog_posts, '_site/blog/index.html',
               list_layout, item_layout, blog='blog', title='Blog', **params)
-    make_list(news_posts, '_site/news/index.html',
-              list_layout, item_layout, blog='news', title='News', **params)
 
     # Create RSS feeds.
     make_list(blog_posts, '_site/blog/rss.xml',
               feed_xml, item_xml, blog='blog', title='Blog', **params)
-    make_list(news_posts, '_site/news/rss.xml',
-              feed_xml, item_xml, blog='news', title='News', **params)
 
 
 # Test parameter to be set temporarily by unit tests.
